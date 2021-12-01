981110d96aa2c1192dfb337b9c2263ee6500645b
Sergei Solovev <sergey.soloviev@inbox.ru>
2021-12-01 03:49:29 +0300

    eleventy blog integration

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,19 @@
 module.exports = function (eleventyConfig) {
     return {
-        templateFormats: ["njk", "md", "ejs", "html", "liquid"],
+        // templateFormats: ["ejs", "md", "html", "liquid"],
     
         // If your site lives in a different subdirectory, change this.
         // Leading or trailing slashes are all normalized away, so don’t worry about it.
         // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
         // This is only used for URLs (it does not affect your file structure)
-        pathPrefix: "/",
-    
-        markdownTemplateEngine: "liquid",
-        htmlTemplateEngine: "njk",
-        dataTemplateEngine: "njk",
+        // pathPrefix: "/",
+
+        // markdownTemplateEngine: "liquid",
         dir: {
-          input: ".",
-          includes: "_includes",
+          input: "src",
+          includes: "../_includes",
           data: "_data",
-          output: "."
+          output: "_site"
         }
       };
     
--- build-scripts/download-articles.mjs
+++ build-scripts/download-articles.mjs
@@ -10,13 +10,19 @@ import NotionPageToHtml from "notion-page-to-html";
 // using async/await
 async function getPage(url) {
   let { title, icon, cover, html } = await NotionPageToHtml.convert(url);
-  const path = `./articles/${title.replace(/ /g, '-')}.html`;
-  html = html.replace('<head>', '<head><link rel="stylesheet" href="/css/global.css" />')
-  await fs.writeFile(path, html)
+  const path = `./src/articles/${title.replace(/ /g, '-')}.ejs`;
+  html = html.replace('<head>', '<head><% include /_head.ejs %>');
+  html = html.replace('<body>', '<body><% include /_page-fade-in-animation.ejs %><% include /_nav.ejs %><main style="overflow: hidden;" class="container">');
+  html = html.replace('</body>', '</main><% include /_footer.ejs %><% include /_scripts.ejs %></body>', '</body>');
+  html = html.replace('margin: 2em auto;', '');
+  html = html.replace('max-width: 900px;', '');
+  html = html.replace('class="page-title"', 'class="page-title" style="margin-top: calc(var(--indent) * 3)"');
+  await fs.writeFile(path, html);
 }
 
 async function main() {
-  const articlesUrls = JSON.parse(await fs.readFile('./articles/article-links.json'))
+  await fs.mkdir('./articles', {recursive: true});
+  const articlesUrls = JSON.parse(await fs.readFile('./src/articles/article-links.json'))
   console.log(articlesUrls);
   await Promise.all(articlesUrls.map(url => getPage(url)));
 }
